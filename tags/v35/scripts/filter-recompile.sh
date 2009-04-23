bb5de00c3caef92c36e41b1e5ee80b4dfb405ecc
alankila <alankila@75748be7-0387-4326-94c7-f41ce0883d2c>
2009-04-23 17:08:01 +0000

    Tag V35 release.

---
+++ tags/v35/scripts/filter-recompile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+MYDIR=$(dirname $0)
+cd "$MYDIR/../sidplay-libs-2.1.1"
+
+# compile
+DEB_BUILD_OPTIONS="debug nostrip" MAKEFLAGS=-j3 fakeroot debian/rules binary-arch
+
+# install
+cd ..
+sudo dpkg -i *.deb
