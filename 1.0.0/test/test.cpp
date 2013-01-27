29cafee1cb2920687590d8c7e8818260307fbc3e
drfiemost <drfiemost@75748be7-0387-4326-94c7-f41ce0883d2c>
2013-01-27 10:24:51 +0000

    Tagging version 1.0.0

---
+++ 1.0.0/test/test.cpp
@@ -0,0 +1,135 @@
+/*
+ * This file is part of libsidplayfp, a SID player engine.
+ *
+ * Copyright 2011-2013 Leandro Nini <drfiemost@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <string>
+#include <iostream>
+#include <memory>
+
+#include "sidplayfp/sidplayfp.h"
+#include "sidplayfp/SidTune.h"
+#include "sidplayfp/sidbuilder.h"
+#include "sidplayfp/sidemu.h"
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+/*
+* Adjust these paths to point to existing ROM dumps
+*/
+#define KERNAL_PATH "/usr/local/lib/vice/C64/kernal"
+#define BASIC_PATH "/usr/local/lib/vice/C64/basic"
+#define CHARGEN_PATH "/usr/local/lib/vice/C64/chargen"
+
+class NullSID: public sidemu
+{
+public:
+    NullSID () : sidemu(0) { m_buffer = 0; }
+
+    void    reset (uint8_t) {}
+    uint8_t read  (uint_least8_t) { return 0; }
+    void    write (uint_least8_t, uint8_t) {}
+    void    clock() {}
+    const   char *credits () const { return ""; }
+    void    voice (const unsigned int num, const bool mute) {}
+    void    model    (SidConfig::sid_model_t model)  {}
+    const   char *error   () const { return ""; }
+    bool    lock     (EventContext *env) { return true; }
+    void    unlock   () {}
+};
+
+class FakeBuilder: public sidbuilder
+{
+private:
+    NullSID sidobj;
+
+public:
+    FakeBuilder  (const char * const name)
+      : sidbuilder(name) { sidobjs.insert (new NullSID()); }
+    ~FakeBuilder () {}
+
+    const char *credits () const { return ""; }
+    void        filter (const bool enable) {}
+    unsigned int availDevices() const { return 0; }
+    unsigned int create(unsigned int) { return 1; }
+};
+
+void loadRom(const char* path, char* buffer)
+{
+    std::ifstream is(path, std::ios::binary);
+    is.read(buffer, 8192);
+    is.close();
+}
+
+int main(int argc, char* argv[])
+{
+    sidplayfp m_engine;
+
+    char kernal[8192];
+    char basic[8192];
+    char chargen[4096];
+
+    loadRom(KERNAL_PATH, kernal);
+    loadRom(BASIC_PATH, basic);
+    loadRom(CHARGEN_PATH, chargen);
+
+    m_engine.setRoms((const uint8_t*)kernal, (const uint8_t*)basic, (const uint8_t*)chargen);
+
+    std::auto_ptr<FakeBuilder> rs(new FakeBuilder("Test"));
+
+    std::string name(PC64_TESTSUITE);
+
+    if (argc > 1)
+    {
+        name.append(argv[1]).append(".prg");
+    }
+    else
+    {
+        name.append(" start.prg");
+    }
+
+    std::auto_ptr<SidTune> tune(new SidTune(name.c_str()));
+
+    if (!tune->getStatus())
+    {
+        std::cerr << "Error: " << tune->statusString() << std::endl;
+        return -1;
+    }
+
+    SidConfig cfg;
+    cfg.frequency = 48000;
+    cfg.samplingMethod = SidConfig::INTERPOLATE;
+    cfg.playback = SidConfig::MONO;
+    cfg.sidEmulation = rs.get();
+    m_engine.config(cfg);
+
+    tune->selectSong(0);
+
+    if (!m_engine.load(tune.get()))
+    {
+        std::cerr <<  m_engine.error() << std::endl;
+        return -1;
+    }
+
+    for (;;)
+    {
+        m_engine.play(0, 48000);
+    }
+}
---
+++ 1.0.0/utils/iniParser.cpp
@@ -0,0 +1,111 @@
+/*
+ *  Copyright (C) 2010-2011 Leandro Nini
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "iniParser.h"
+
+#include <stdlib.h>
+
+std::string iniParser::parseSection(const char *buffer)
+{
+    std::string section(buffer);
+    section.erase(0, 1);
+    section.erase(section.find(']'));
+    return section;
+}
+
+std::pair<std::string, std::string> iniParser::parseKey(const char *buffer)
+{
+    std::string section(buffer);
+    size_t pos = section.find('=');
+
+    if (pos == std::string::npos)
+    {
+        throw emptyPair();
+    }
+
+    std::string key = section.substr(0, pos);
+    std::string value = section.substr(pos + 1);
+    //Trim right spaces
+    key.erase(key.find_last_not_of(' ') + 1);
+    return make_pair(key, value);
+}
+
+#define BUFSIZE 2048
+
+bool iniParser::open(const char *fName)
+{
+    std::ifstream iniFile(fName);
+
+    if (iniFile.fail())
+    {
+        return false;
+    }
+
+    std::map<std::string, keys_t>::iterator mIt;
+
+    while (iniFile.good())
+    {
+        char buffer[BUFSIZE];
+        iniFile.getline(buffer, BUFSIZE);
+
+        switch (buffer[0])
+        {
+        case ';':
+        case '#':
+            // skip comments
+            break;
+
+        case '[':
+        {
+            std::string section = parseSection(buffer);
+            keys_t keys;
+            std::pair<std::map<std::string, keys_t>::iterator, bool> it = sections.insert(make_pair(section, keys));
+            mIt = it.first;
+        }
+        break;
+
+        default:
+            try
+            {
+                (*mIt).second.insert(parseKey(buffer));
+            }
+            catch (emptyPair &e) {};
+
+            break;
+        }
+    }
+
+    return true;
+}
+
+void iniParser::close()
+{
+    sections.clear();
+}
+
+bool iniParser::setSection(const char *section)
+{
+    curSection = sections.find(std::string(section));
+    return (curSection != sections.end());
+}
+
+const char *iniParser::getValue(const char *key)
+{
+    std::map<std::string, std::string>::const_iterator keyIt = (*curSection).second.find(std::string(key));
+    return (keyIt != (*curSection).second.end()) ? keyIt->second.c_str() : 0;
+}
