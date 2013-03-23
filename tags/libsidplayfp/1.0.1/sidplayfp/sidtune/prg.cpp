979c9fa9adb09b2a71ac2a68447dfc161752fe99
drfiemost <drfiemost@75748be7-0387-4326-94c7-f41ce0883d2c>
2013-03-23 10:51:45 +0000

    Tagging version 1.0.1

---
+++ tags/libsidplayfp/1.0.1/sidplayfp/sidtune/prg.cpp
@@ -0,0 +1,67 @@
+/*
+ * This file is part of libsidplayfp, a SID player engine.
+ *
+ * Copyright 2011-2013 Leandro Nini <drfiemost@users.sourceforge.net>
+ * Copyright 2007-2010 Antti Lankila
+ * Copyright 2000-2001 Simon White
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "prg.h"
+
+#include <memory>
+
+#include "SidTuneCfg.h"
+#include "SidTuneTools.h"
+#include "SidTuneInfoImpl.h"
+
+const char TXT_FORMAT_PRG[] = "Tape image file (PRG)";
+
+const char ERR_TRUNCATED[]  = "ERROR: File is most likely truncated";
+
+
+SidTuneBase* prg::load(const char *fileName, Buffer_sidtt<const uint_least8_t>& dataBuf)
+{
+    const char *ext = SidTuneTools::fileExtOfPath(fileName);
+    if ( (MYSTRICMP(ext,".prg")!=0) &&
+         (MYSTRICMP(ext,".c64")!=0) )
+    {
+        return 0;
+    }
+
+    if (dataBuf.len() < 2)
+    {
+        throw loadError(ERR_TRUNCATED);
+    }
+
+    std::auto_ptr<prg> tune(new prg());
+    tune->load();
+
+    return tune.release();
+}
+
+void prg::load()
+{
+    info->m_formatString = TXT_FORMAT_PRG;
+
+    // Automatic settings
+    info->m_songs         = 1;
+    info->m_startSong     = 1;
+    info->m_compatibility = SidTuneInfo::COMPATIBILITY_BASIC;
+
+    // Create the speed/clock setting table.
+    convertOldStyleSpeedToTables(~0, info->m_clockSpeed);
+}
